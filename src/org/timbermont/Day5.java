44285cd64be51ef3d496a6588c28a2a26889be6c
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2020-12-06 21:25:22 +0100

    Day 5 part 2

--- src/org/timbermont/Day5.java
+++ src/org/timbermont/Day5.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.OptionalLong;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,11 +19,29 @@ public class Day5 {
         System.out.println("Part 1:");
         final OptionalLong highestId = inputSeats.stream().mapToLong(Seat::getID).max();
         System.out.println("Highest ID: " + highestId);
+
+        System.out.println("\nPart 2:");
+        final TreeSet<Long> seatIds = inputSeats.stream().map(Seat::getID).collect(Collectors.toCollection(TreeSet::new));
+        long mySeat = getMySeat(seatIds);
+        System.out.println("My seat ID: " + mySeat);
     }
 
     static void loadInput() throws URISyntaxException, IOException {
         final Stream<String> lines = Files.lines(Path.of(Day1.class.getResource("Day5_input.txt").toURI()));
         inputSeats = lines.map(Seat::new).collect(Collectors.toList());
+    }
+
+    private static long getMySeat(TreeSet<Long> seatIds) {
+        long mySeat = -1;
+        for (long i = 0; i < seatIds.last(); i++) {
+            if (!seatIds.contains(i)) { // cannot be a taken seat
+                if (seatIds.contains(i - 1) && seatIds.contains(i + 1)) { // seats needs two neighboring taken seats
+                    mySeat = i;
+                    break;
+                }
+            }
+        }
+        return mySeat;
     }
 
     private static class Seat {
