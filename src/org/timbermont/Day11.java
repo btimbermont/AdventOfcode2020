09728061895eb24a462d33fa6c4f5e66f0b09f01
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2020-12-17 00:44:56 +0100

    Day 11 part 2

--- src/org/timbermont/Day11.java
+++ src/org/timbermont/Day11.java
@@ -6,8 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static java.lang.Math.*;
-
 public class Day11 {
 
     static final char EMPTY = 'L';
@@ -22,6 +20,18 @@ public class Day11 {
         System.out.println("Part 1:");
         int i = 0;
         while (updateFloorPlan()) {
+            i++;
+        }
+        System.out.println("Updated " + i + " times before stabilizing to this:");
+        printFloorPlan();
+        System.out.println("Free seats: " + countTakenSeats());
+
+        System.out.println("\n\nPart 2: ");
+        //reset floorplan
+        loadInput();
+
+        i = 0;
+        while (updateFloorplan2()) {
             i++;
         }
         System.out.println("Updated " + i + " times before stabilizing to this:");
@@ -60,20 +70,74 @@ public class Day11 {
 
     private static int takenAdjacentSeats(final int i, final int j) {
         int takenSeats = 0;
-        for (int a = max(0, i - 1); a <= min(i + 1, floorPlan.length - 1); a++) {
-            for (int b = max(0, j - 1); b <= min(j + 1, floorPlan[a].length - 1); b++) {
-                if (i == a && j == b) continue;
-                if (floorPlan[a][b] == TAKEN) takenSeats++;
+        for (int x = i - 1; x <= i + 1; x++) {
+            for (int y = j - 1; y <= j + 1; y++) {
+                if (isOutOfBounds(x, y) || (i == x && j == y)) continue;
+                if (floorPlan[x][y] == TAKEN) takenSeats++;
             }
         }
         return takenSeats;
     }
 
-    private static int countTakenSeats(){
+    private static boolean updateFloorplan2() {
+        char[][] newFloorplan = copyFloorplan();
+        boolean somethingChanged = false;
+        for (int i = 0; i < floorPlan.length; i++) {
+            for (int j = 0; j < floorPlan[i].length; j++) {
+                switch (floorPlan[i][j]) {
+                    case EMPTY:
+                        if (takenVisibleSeats(i, j) == 0) {
+                            newFloorplan[i][j] = TAKEN;
+                            somethingChanged = true;
+                        }
+                        break;
+                    case TAKEN:
+                        if (takenVisibleSeats(i, j) >= 5) {
+                            newFloorplan[i][j] = EMPTY;
+                            somethingChanged = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        if (somethingChanged) {
+            floorPlan = newFloorplan;
+        }
+        return somethingChanged;
+    }
+
+    private static int takenVisibleSeats(final int i, final int j) {
+        int visibleTakenSeats = 0;
+        for (int dirX = -1; dirX <= 1; dirX++) {
+            for (int dirY = -1; dirY <= 1; dirY++) {
+                if (dirX == 0 && dirY == 0) continue;
+                if (seesTakenSeatInDirection(i, j, dirX, dirY)) {
+                    visibleTakenSeats++;
+                }
+            }
+        }
+        return visibleTakenSeats;
+    }
+
+    private static boolean seesTakenSeatInDirection(final int posX, final int posY, final int dirX, final int dirY) {
+        final int watchX = posX + dirX;
+        final int watchY = posY + dirY;
+        if (isOutOfBounds(watchX, watchY) || floorPlan[watchX][watchY] == EMPTY) return false;
+        if (floorPlan[watchX][watchY] == TAKEN) return true;
+        return seesTakenSeatInDirection(watchX, watchY, dirX, dirY);
+    }
+
+    private static boolean isOutOfBounds(int watchX, int watchY) {
+        return watchX < 0 || watchX >= floorPlan.length || watchY < 0 || watchY >= floorPlan[watchX].length;
+    }
+
+    private static int countTakenSeats() {
         int occupied = 0;
         for (char[] row : floorPlan) {
             for (char position : row) {
-                if(position == TAKEN) occupied++;
+                if (position == TAKEN) occupied++;
             }
         }
         return occupied;
